4d58376ff854532eeddfce95fc6aebd541795f95
suoeh <dylanydai@gmail.com>
2023-08-19 17:38:21 -0400

    BUSSS

--- backend/instructions.py
+++ backend/instructions.py
@@ -1,97 +1,52 @@
-import csv
 import json
+import requests
 
-categories = []
-names = []
 items = []
 
 file = open("backend\WasteData.json", 'r')
 data = json.load(file)
 
-
-import json
-import requests
-
 API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
 api_key = "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"
 headers = {"Authorization": f"Bearer {api_key}"}
-
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-
 for rows in data:
-    items += list(map(lambda x : x.strip(), rows["keywords"].split(',')))
-
-# print(items)
-for n, rows in enumerate(data):
-    if n == 0:
-        continue
-    categories.append(rows["category"])
-    temp = rows["category"]
-    
-    try:
-        temp = temp[temp.index('(') + 1:temp.index(')')]
-    except ValueError:
-        pass
-    names.append(temp)
-
-# print(categories)
-
-# exit()
-
-# def find(name):
-#     file = open("backend\WasteData.csv", 'r')
-#     data = csv.reader(file)
-
-#     for n, rows in enumerate(data):
-
-#         garbage = list(rows[3].split(','))
-        
-#         for i in garbage:
-           
-#             if len(i) < len(name):
-#                 continue
-
-#             if i[1:len(name) + 1] == name or i[:len(name)] == name or i[len(i) - len(name):] == name or i[len(i) - len(name) - 1:-2] == name:
-#                 print(i)
-#                 print("reached")
-#                 return n
-                
-#     return 97
+    items += list(rows["keywords"].split(','))
 
 def find(name):
-    data = query(
+    global items
+
+    querydata = query(
     {
         "inputs": {
             "source_sentence": name,
             "sentences":items
         }
     })
-    print(items[data.index(max(data))])
-    print(max(data))
+    
+    ind = querydata.index(max(querydata))
+    
+    if max(querydata) < 0.6:
+        return 97 + 97
+    
+    print(items[ind])
 
-find("Cigarette (Cigarette) Cigarette Butts")
+    for n in range(len(data)):
+        garbage = list(data[n]["keywords"].split(','))
+
+        if items[ind] in garbage:
+            return n
+                
+    return 97 + 97
 
 def translate(row):
-    file = open("backend\WasteData.csv", 'r')
-    data = csv.reader(file)
     
-    for i in range(1, 196):
-        
-        if i < row:
-            next(data)
-            continue
-        
-        row = next(data)
-        print(row[0])
-        print(row[2])
-        
-        break
+    print(data[row]["category"])
+    print(data[row]["body"])
+    
 
-def filter(arr):
-    test = arr
-    for i in arr:
-        find(i)
+translate(find("paper"))
