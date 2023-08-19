439855cae34bc884c3d3afde78c95ed38cdd58b4
suoeh <dylanydai@gmail.com>
2023-08-19 14:18:58 -0400

    Update instructions.py

--- backend/instructions.py
+++ backend/instructions.py
@@ -2,40 +2,67 @@ import csv
 
 categories = []
 names = []
-bins = set()
+items = []
 
 file = open("backend\WasteData.csv", 'r')
 data = csv.reader(file)
 
+for rows in data:
+        items += list(rows[3].split(','))
+
+print(items)
+exit()
 for n, rows in enumerate(data):
     if n == 0:
         continue
     categories.append(rows[0])
-    temp = [rows[0]] * 2
+    temp = rows[0]
+    
     try:
-        temp = [temp[0][temp.index('(') + 1:temp[0].index(')')], temp[0][:temp[0].index('(') - 1]]
+        temp = temp[temp.index('(') + 1:temp.index(')')]
     except ValueError:
         pass
-    names.append(temp[0])
-    bins.add(temp[1])
+    names.append(temp)
 
-print(names)
-print(bins)
-# print(categories)
+print(categories)
 
-def read(object):
+def find(name):
+    file = open("backend\WasteData.csv", 'r')
+    data = csv.reader(file)
+
+    for n, rows in enumerate(data):
+
+        garbage = list(rows[3].split(','))
+        
+        for i in garbage:
+           
+            if len(i) < len(name):
+                continue
+
+            if i[1:len(name) + 1] == name or i[:len(name)] == name or i[len(i) - len(name):] == name or i[len(i) - len(name) - 1:-2] == name:
+                print(i)
+                print("reached")
+                return n
+                
+    return 97
+
+def translate(row):
     file = open("backend\WasteData.csv", 'r')
     data = csv.reader(file)
     
-    for n, rows in enumerate(data):
-        if n == categories.index(object) + 1:
-            print(rows[2])
-            print(2)
-            break
-        continue
+    for i in range(1, 196):
+        
+        if i < row:
+            next(data)
+            continue
+        
+        row = next(data)
+        print(row[0])
+        print(row[2])
+        
+        break
 
-    object = object[:object.index('(') - 1]
-    
-    return object
-
-print(read('Blue Bin (aluminum)'))
+def filter(arr):
+    test = arr
+    for i in arr:
+        find(i)
